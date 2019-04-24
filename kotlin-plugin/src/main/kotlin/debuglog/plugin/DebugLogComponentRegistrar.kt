52f5618f93ee8f7ceaf7a592b3076ce855f29d93
andrei_papko <andrei_papko@epam.com>
2019-04-24 13:45:34 +0300

    add log / no crash

--- kotlin-plugin/src/main/kotlin/debuglog/plugin/DebugLogComponentRegistrar.kt
+++ kotlin-plugin/src/main/kotlin/debuglog/plugin/DebugLogComponentRegistrar.kt
@@ -1,6 +1,9 @@
 package debuglog.plugin
 
 import com.google.auto.service.AutoService
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.codegen.extensions.ClassBuilderInterceptorExtension
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
@@ -15,13 +18,21 @@ class DebugLogComponentRegistrar : ComponentRegistrar {
     if (configuration[KEY_ENABLED] == false) {
       return
     }
-    ClassBuilderInterceptorExtension.registerExtension(
-        project,
-        DebugLogClassGenerationInterceptor(
-            debugLogAnnotations = configuration[KEY_ANNOTATIONS]
-                ?: error("debuglog plugin requires at least one annotation class option passed to it")
-        )
+
+    val messageCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
+
+    messageCollector.report(
+            CompilerMessageSeverity.WARNING,
+            "!!! DebugLogComponentRegistrar execute !!!"
     )
+
+//    ClassBuilderInterceptorExtension.registerExtension(
+//        project,
+//        DebugLogClassGenerationInterceptor(
+//            debugLogAnnotations = configuration[KEY_ANNOTATIONS]
+//                ?: error("debuglog plugin requires at least one annotation class option passed to it")
+//        )
+//    )
     // TODO: IrGenerationExtension.registerExtension for Kotlin Native :)
   }
 }
